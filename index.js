ee094221f56220d335b865f667a3f37b0b08b3ef
NikhatKotwal <84654224+NikhatKotwal@users.noreply.github.com>
2022-10-15 16:28:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+// var arr = ["nikhat","hello","hii","45","hey"];
+// console.log(arr[2])
+
+var Dividend = 2 ;
+var Divisor = 3;
+var Quotient = 4;
+var Remainder = 1 ;
+var Quotient = Math.floor(Dividend/ Divisor) ;
+var Remainder = Dividend% Divisor;
+console.log(Quotient ,Remainder)
+
+// Addtion..........
+
+function add(a,b){
+    var ans=a+b;
+    return ans
+}
+console.log(add(10 , 4))
+
+//Substraction...........
+
+function sub(a,b){
+    var ans=a - b;
+    return ans
+}
+console.log(sub(10 , 4))
+
+
+//Multiplications..........
+
+function Multiplications(a,b){
+    var ans=a * b;
+    return ans
+}
+console.log(Multiplications(10 , 4))
+
+//Divisor.......
+function Div(a,b){
+    var ans = a/b;
+    return ans
+}
+console.log(Div(10,4))
